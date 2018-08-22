dfce613fa3be9fc9135ed728fef7d58e95df5146
xuzhu <731333977@qq.com>
2018-08-22 22:50:48 +0800

    第一次上传vue工程

---
+++ src/components/Test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="didi-navigator">
+    <ul>
+      <!--<li v-for="tab in tabs" :value="tab.t" :key="tab.x">-->
+        <li v-for="tab in tabs" :value="tab.text" :key="tab.text">
+          {{tab.text}}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+  export default {
+    Test: "/Test",
+    data() {
+      return {
+        tabs: [{
+            text: "巴士"
+          },
+          {
+            text: "快车"
+          },
+          {
+            text: "专车"
+          },
+          {
+            text: "顺风车"
+          },
+          {
+            text: "出租车"
+          },
+          {
+            text: "代驾"
+          }
+        ]
+      };
+    }
+  };
+
+</script>
